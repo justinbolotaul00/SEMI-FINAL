2784457f14f6f992cd064bde8b8b091f36458176
justinbolotaul00 <bolotaulo.justinmatthew@cec.edu.ph>
2025-05-06 03:01:01 +0800

    third commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello World, from Flask'
+    return 'Hello World, from aquaFlask'
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
